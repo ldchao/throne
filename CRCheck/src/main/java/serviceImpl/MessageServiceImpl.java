3ac45d6cc7c203a3e7b100db8768b5f9a846b238
ldc14 <ldc14@software.nju.edu.cn>
2016-07-12 10:47:45 +0800

    删无用文件

--- CRCheck/src/main/java/serviceImpl/MessageServiceImpl.java
+++ CRCheck/src/main/java/serviceImpl/MessageServiceImpl.java
@@ -1,7 +1,11 @@
 package serviceImpl;
 
+import Dao.MessageDao;
+import DaoImpl.MessageDaoImpl;
+import POJO.Message;
 import enums.MessageState;
 import enums.UniversalState;
+import model.InvitationMessage;
 import model.ProjectModel;
 import service.MessageService;
 
@@ -11,9 +15,21 @@ import service.MessageService;
 public class MessageServiceImpl implements MessageService {
     public UniversalState setIssueMessage(ProjectModel projectModel) {
 
-        String message="";
-        message+="您的好友"+projectModel.getUserID();
-
+        MessageDao messageDao=new MessageDaoImpl();
+        String constantMessage="";
+        constantMessage+=projectModel.getUserID()+"邀请您于"+projectModel.getStartDate()+"至"
+                +projectModel.getEndDate()+"参加他发起的"+"“"+projectModel.getName()
+                +"”项目的评审，请查收消息。";
+        boolean result=true;
+        for (InvitationMessage invitationMessage:projectModel.getInvitationList()) {
+              String invitement="尊敬的用户您好，"+constantMessage;
+            Message message=new Message();
+            message.setContent(invitement);
+            message.setUserId(invitationMessage.getUserID());
+            message.setState(MessageState.NotHandle.toString());
+            message.setSendOrReceive("receive");
+            result=result&messageDao.addMessage(message);
+        }
 
         return null;
     }
