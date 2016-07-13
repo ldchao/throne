68578c87db7b68536d7d518fe26718182eb8435d
zs14 <zs14@software.nju.edu.cn>
2016-07-13 16:57:51 +0800

    user

--- CRCheck/src/main/java/service/UserService.java
+++ CRCheck/src/main/java/service/UserService.java
@@ -2,11 +2,13 @@ package service;
 
 import model.UserModel;
 
+import java.util.List;
+
 /**
  * Created by lvdechao on 2016/7/10.
  */
 public interface UserService {
 
     public UserModel getUser(String userid);
-
+    public List<String> getUserList();
 }
--- CRCheck/src/main/java/serviceImpl/UserServiceImpl.java
+++ CRCheck/src/main/java/serviceImpl/UserServiceImpl.java
@@ -8,6 +8,10 @@ import model.UserModel;
 import service.MessageService;
 import service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by zs on 2016/7/10.
  */
@@ -27,12 +31,29 @@ public class UserServiceImpl implements UserService {
         return model;
     }
 
+    public List<String> getUserList(){
+        UserDao dao=new UserDaoImpl();
+        List list=dao.getAllUserId();
+        if(list.size()<=8)
+            return list;
+        Random rand = new Random();
+        int randNum = rand.nextInt(list.size()-8);
+        List<String> result=new ArrayList<String>();
+        for(int i=randNum;i<randNum+8;i++){
+            result.add(list.get(i)+"");
+        }
+        return result;
+    }
+
     //测试
     public static void main(String[] args) {
         UserService u=new UserServiceImpl();
-        UserModel state=u.getUser("saige");
-        System.out.print(String.valueOf(state.getId()));
-        System.out.print(String.valueOf(state.getPower()));
+        //UserModel state=u.getUser("saige");
+        List<String> list=u.getUserList();
+        for(String s:list)
+            System.out.print(s+" ");
+//        System.out.print(String.valueOf(state.getId()));
+//        System.out.print(String.valueOf(state.getPower()));
     }
 
 }
