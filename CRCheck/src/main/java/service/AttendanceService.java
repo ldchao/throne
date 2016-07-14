8825e4274e0aa36cc3d81d6fbef8e6d9e41d1a5a
ldc14 <ldc14@software.nju.edu.cn>
2016-07-14 23:55:47 +0800

    得到参加项目和发起项目接口

--- CRCheck/src/main/java/service/AttendanceService.java
+++ CRCheck/src/main/java/service/AttendanceService.java
@@ -1,9 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+
 /**
  * Created by lvdechao on 2016/7/14.
  */
 public interface AttendanceService {
-    
+
+    // TODO: 2016/7/14 查看我发起的项目列表
+    public ArrayList<String> getOwnProjectID(String userID);
+
+
+    // TODO: 2016/7/14 查看我参与的项目列表
+    public ArrayList<String> getAttendProjectID(String userID);
 
 }
---
+++ CRCheck/src/main/java/serviceImpl/AttendanceServiceImpl.java
@@ -0,0 +1,19 @@
+package serviceImpl;
+
+import service.AttendanceService;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lvdechao on 2016/7/14.
+ */
+public class AttendanceServiceImpl implements AttendanceService{
+
+    public ArrayList<String> getOwnProjectID(String userID) {
+        return null;
+    }
+
+    public ArrayList<String> getAttendProjectID(String userID) {
+        return null;
+    }
+}
