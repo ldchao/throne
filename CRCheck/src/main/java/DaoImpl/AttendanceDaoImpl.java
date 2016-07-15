9f076ec219cc58f72d49fc097ada99e91aa1c0da
dd212688 <983117808@qq.com>
2016-07-15 10:47:49 +0800

    debug

--- CRCheck/src/main/java/DaoImpl/AttendanceDaoImpl.java
+++ CRCheck/src/main/java/DaoImpl/AttendanceDaoImpl.java
@@ -137,11 +137,8 @@ public class AttendanceDaoImpl  implements AttendanceDao {
             Query query = session.createQuery(hql);
             ArrayList<Attendance> aList =(ArrayList<Attendance>) query.list();
             connection.closeSession(session);
-            if(aList.size()==0){
-                return null;
-            }else{
                 return aList;
-            }
+
         }catch (Exception e){
             e.printStackTrace();
             connection.closeSession(session);
@@ -156,11 +153,8 @@ public class AttendanceDaoImpl  implements AttendanceDao {
             Query query = session.createQuery(hql);
             ArrayList<Attendance> aList =(ArrayList<Attendance>) query.list();
             connection.closeSession(session);
-            if(aList.size()==0){
-                return null;
-            }else{
                 return aList;
-            }
+
         }catch (Exception e){
             e.printStackTrace();
             connection.closeSession(session);
