58f70746671b0738e1864cff60522736cd486855
zs14 <zs14@software.nju.edu.cn>
2016-07-17 09:40:39 +0800

    评审交互

--- CRCheck/src/main/java/controller/ReviewController.java
+++ CRCheck/src/main/java/controller/ReviewController.java
@@ -2,13 +2,13 @@ package controller;
 
 import enums.UniversalState;
 import model.PersonalReviewRecord;
+import model.SummaryReviewRecord;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.ReviewRecordService;
 import serviceImpl.ReviewRecordServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +45,24 @@ public class ReviewController {
     public List<PersonalReviewRecord> getPersonReviewList(String userID, int projectID){
         ReviewRecordService review=new ReviewRecordServiceImpl();
         List<PersonalReviewRecord> list=review.checkPersonalReviewRecord(userID,projectID);
+        return list;
+    }
+
+    //查看参评所有完成评审用户名单
+    @RequestMapping(value = "/getFinishUserList", method = RequestMethod.POST)
+    @ResponseBody
+    public List<String> getFinishUserList(int projectID){
+        ReviewRecordService review=new ReviewRecordServiceImpl();
+        List<String> list=review.checkProjectUserList(projectID);
+        return list;
+    }
+
+    //查看评审记录
+    @RequestMapping(value = "/getFinishUserList", method = RequestMethod.POST)
+    @ResponseBody
+    public List<SummaryReviewRecord> checkSummaryReview(int projectID){
+        ReviewRecordService review=new ReviewRecordServiceImpl();
+        List<SummaryReviewRecord> list=review.checkSummaryReviewRecord(projectID);
         return list;
     }
 }
