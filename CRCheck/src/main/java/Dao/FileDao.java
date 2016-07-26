75897207f1f8a07b1e734dc5fc67fbf3cdf81451
lxbin14 <lxbin14@software.nju.edu.cn>
2016-07-26 18:08:26 +0800

    傻逼超删掉我的

--- CRCheck/src/main/java/Dao/FileDao.java
+++ CRCheck/src/main/java/Dao/FileDao.java
@@ -1,14 +1,14 @@
-//package Dao;
-//
-//import POJO.File;
-//
-///**
-// * Created by mm on 2016/7/26.
-// */
-//public interface FileDao {
-//    //need the "path" of "File",it will return the "state" of "File"
-//    public String getFileState(File po);
-//
-//    //need the "path" of "File",it will return the "lastTime" of "File"
-//    public String getFileLastTime(File po);
-//}
+package Dao;
+
+import POJO.File;
+
+/**
+ * Created by mm on 2016/7/26.
+ */
+public interface FileDao {
+    //need the "path" of "File",it will return the "state" of "File"
+    public String getFileState(File po);
+
+    //need the "path" of "File",it will return the "lastTime" of "File"
+    public String getFileLastTime(File po);
+}
