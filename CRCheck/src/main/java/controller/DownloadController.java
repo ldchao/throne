c1e0a0f99f84a95f5b1fa05ce435dc1bbee304fd
ldc14 <ldc14@software.nju.edu.cn>
2016-07-26 15:51:47 +0800

    文件上传下载

---
+++ CRCheck/src/main/java/controller/DownloadController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+public class DownloadController {
+
+	/**
+	 *
+	 * 将fileName放到session中
+	 * 如果下载整个项目的zip，传输 fileName= ProjectCompressedFile/"+项目存储编号+"/文件名"
+	 * 否则传输 fileName= ProjectDecompressedFile/"+项目存储编号+"/文件具体路径"
+	 *
+     */
+
+	@RequestMapping("/download")
+	public String download(HttpServletRequest request, HttpServletResponse response){
+//		public String download(@RequestParam String fileName , HttpServletRequest request, HttpServletResponse response){
+		String fileName=(String)request.getSession().getAttribute("fileName");
+		response.setContentType("text/html;charset=utf-8");
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+	
+		String ctxPath = request.getSession().getServletContext().getRealPath("/") + "ProjectResources/";
+		String downLoadPath = ctxPath + fileName;
+		System.out.println(downLoadPath);
+		try {
+			long fileLength = new File(downLoadPath).length();
+			response.setContentType("application/x-msdownload;");
+			response.setHeader("Content-disposition", "attachment; filename=" + new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+			response.setHeader("Content-Length", String.valueOf(fileLength));
+			bis = new BufferedInputStream(new FileInputStream(downLoadPath));
+			bos = new BufferedOutputStream(response.getOutputStream());
+			byte[] buff = new byte[2048];
+			int bytesRead;
+			while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+				bos.write(buff, 0, bytesRead);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bis != null)
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (bos != null)
+				try {
+					bos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		return null;
+	
+	}
+	
+}
--- CRCheck/src/main/java/controller/UploadController.java
+++ CRCheck/src/main/java/controller/UploadController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import POJO.User;
+import enums.UniversalState;
+import model.UserModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import service.FileService;
+import serviceImpl.FileServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -17,11 +23,23 @@ import java.util.Map;
 public class UploadController {
 
 	@RequestMapping("/oneUpload")
-	public String oneUpload(@RequestParam("imageFile") MultipartFile imageFile, HttpServletRequest request){
+	public String oneUpload(@RequestParam("oneFile") MultipartFile oneFile, @RequestParam("projectId") String projectId,HttpServletRequest request){
 
-		String uploadUrl = request.getSession().getServletContext().getRealPath("/") + "upload/";
-		String filename = imageFile.getOriginalFilename();
-		
+        // TODO: 2016/7/26 取id ，用来创建文件名，返回id用来存储
+		String fileId =projectId;
+
+		String realPath=request.getSession().getServletContext().getRealPath("/");
+		String uploadUrl = realPath + "ProjectResources/ProjectCompressedFile/"+fileId+"/";
+		String decompressionUrl=realPath+"ProjectResources/ProjectDecompressedFile/"+fileId+"/";
+		String filename = oneFile.getOriginalFilename();
+
+//		String originalName = oneFile.getOriginalFilename();
+//		String nameSplit[]=originalName.split("\\.");
+//		int index=nameSplit.length-1;
+//		String filename =fileId;
+//		if(index>0)
+//			filename+="."+nameSplit[index];
+		//文件夹不存在时新建文件夹
 		File dir = new File(uploadUrl);
 		if (!dir.exists()) {
 			dir.mkdirs();
@@ -34,37 +52,84 @@ public class UploadController {
 			try {
 				targetFile.createNewFile();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
 		try {
-			imageFile.transferTo(targetFile);
+			oneFile.transferTo(targetFile);
 		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return "redirect:http://localhost:8082/upload/" + filename;
+
+		FileService fileService=new FileServiceImpl();
+		fileService.unZip( uploadUrl + filename,decompressionUrl);
+
+		return null;
 	}
-	
+
+	@RequestMapping("/headPortraitsUpload")
+	@ResponseBody
+	public String headPortraitsUpload(@RequestParam("imageFile") MultipartFile imageFile, HttpServletRequest request){
+
+		UserModel user=(UserModel)request.getSession().getAttribute("User");
+		String uploadUrl = request.getSession().getServletContext().getRealPath("/") + "HeadPortraits/";
+
+		String originalName = imageFile.getOriginalFilename();
+		String nameSplit[]=originalName.split("\\.");
+		int index=nameSplit.length-1;
+		String filename =user.getId();
+		if(index>0)
+			filename+="."+nameSplit[index];
+
+		File dir = new File(uploadUrl);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+
+		System.out.println("文件上传到: " + uploadUrl + filename);
+
+		File targetFile = new File(uploadUrl + filename);
+		if (!targetFile.exists()) {
+			try {
+				targetFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return UniversalState.FAIL.toString();
+			}
+		}
+
+		try {
+			imageFile.transferTo(targetFile);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			return UniversalState.FAIL.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return UniversalState.FAIL.toString();
+		}
+
+		return UniversalState.SUCCESS.toString();
+	}
+
 	@RequestMapping("/moreUpload")
-	public String moreUpload(HttpServletRequest request){
-		
+	public String moreUpload(HttpServletRequest request,@RequestParam("projectId") String projectId){
+
+		String fileId =projectId;
+
 		MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
 		Map<String, MultipartFile> files = multipartHttpServletRequest.getFileMap();
 		
-		String uploadUrl = request.getSession().getServletContext().getRealPath("/") + "upload/";
+		String uploadUrl = request.getSession().getServletContext().getRealPath("/")
+				+ "ProjectResources/ProjectDecompressedFile/"+fileId+"/";
 		File dir = new File(uploadUrl);
 		if (!dir.exists()) {
 			dir.mkdirs();
 		}
 		
-		List<String> fileList = new ArrayList<String>();
+//		List<String> fileList = new ArrayList<String>();
 		
 		for (MultipartFile file :  files.values()) {
 			File targetFile = new File(uploadUrl + file.getOriginalFilename());
@@ -72,27 +137,22 @@ public class UploadController {
 				try {
 					targetFile.createNewFile();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
 				try {
 					file.transferTo(targetFile);
-					fileList.add("http://localhost:8082/upload/" + file.getOriginalFilename());
+//					fileList.add("http://localhost:8082/upload/" + file.getOriginalFilename());
 				} catch (IllegalStateException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
 			}
 		}
-		
-		request.setAttribute("files", fileList);
-		
-		return "moreUploadResult";
+//		request.setAttribute("files", fileList);
+		return null;
 	}
 	
 }
