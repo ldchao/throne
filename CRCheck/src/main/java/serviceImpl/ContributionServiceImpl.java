12f29e56b30109444723a4a5f9f2a0a2d8c82555
ldc14 <ldc14@software.nju.edu.cn>
2016-07-28 16:33:16 +0800

    成就实现类创建

---
+++ CRCheck/src/main/java/serviceImpl/ContributionServiceImpl.java
@@ -0,0 +1,30 @@
+package serviceImpl;
+
+import enums.UniversalState;
+import model.AchievementModel;
+import model.ContributionModel;
+import service.ContributionService;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lvdechao on 2016/7/28.
+ */
+public class ContributionServiceImpl implements ContributionService{
+
+    public UniversalState addContribution(String userID, int projectID) {
+        return null;
+    }
+
+    public ArrayList<ContributionModel> checkContributionList(String userID) {
+        return null;
+    }
+
+    public ContributionModel checkContributionSum(String userID) {
+        return null;
+    }
+
+    public ArrayList<AchievementModel> checkAchievement(String userID) {
+        return null;
+    }
+}
