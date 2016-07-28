50ef11851e283636d70a4d82977ab5f9e7374f23
zs14 <zs14@software.nju.edu.cn>
2016-07-28 11:22:24 +0800

    file

--- CRCheck/src/main/java/controller/FileController.java
+++ CRCheck/src/main/java/controller/FileController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import service.FileService;
 import serviceImpl.FileServiceImpl;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +21,9 @@ public class FileController {
     //获取文件夹内容
     @RequestMapping(value = "/dir/{path}", method = RequestMethod.POST)
     @ResponseBody
-    public List<FileModel> getContent(@PathVariable("path") String path){
+    public List<FileModel> getContent(@PathVariable("path") String path,HttpServletRequest request,int projectId){
+        String pathHeader=request.getSession().getServletContext().getRealPath("/");
+        path=pathHeader+"/"+projectId+"/"+path;
         FileService s=new FileServiceImpl();
         List<FileModel> list=s.getDir(path);
         return list;
@@ -28,9 +31,10 @@ public class FileController {
     //读取文件
     @RequestMapping(value = "/file/{path}")
     @ResponseBody
-    public List<String> getfileContent(@PathVariable("path")String path,String type){
+    public List<String> getfileContent(@PathVariable("path")String path, HttpServletRequest request,int projectId,String type){
+        String pathHeader=request.getSession().getServletContext().getRealPath("/");
         FileService s=new FileServiceImpl();
-        path="D:/"+path+"."+type;
+        path=pathHeader+"/"+projectId+"/"+path+"."+type;
         System.out.println(path);
         List<String> list=s.readFile(path);
         List<String> result=new ArrayList<String>();
