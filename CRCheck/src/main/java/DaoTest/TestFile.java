04545f7e97bb3e5183d34aafa4333b99782ca619
dd212688 <983117808@qq.com>
2016-07-28 10:45:12 +0800

    查看文件debug

---
+++ CRCheck/src/main/java/DaoTest/TestFile.java
@@ -0,0 +1,25 @@
+package DaoTest;
+
+import model.FileModel;
+import service.FileService;
+import serviceImpl.FileServiceImpl;
+
+import java.util.List;
+
+/**
+ * Created by dlydd on 2016/7/28.
+ */
+public class TestFile {
+    public static void main(String args[]){
+        FileService fileService = new FileServiceImpl();
+        List<FileModel> fileModelList= fileService.getDir("C://aaaa");
+        for(int i=0;i<fileModelList.size();i++){
+            FileModel fileModel = fileModelList.get(i);
+            System.out.println(fileModel.getContent());
+            System.out.println(fileModel.getN());
+            System.out.println(fileModel.getPath());
+            System.out.println(fileModel.getTime());
+            System.out.println(fileModel.getType());
+        }
+    }
+}
--- CRCheck/src/main/java/DaoTest/TestProjectquality.java
+++ CRCheck/src/main/java/DaoTest/TestProjectquality.java
@@ -13,10 +13,10 @@ public class TestProjectquality {
         Projectquality projectquality = new Projectquality();
         projectquality.setDescription("good");
         projectquality.setEndTime("10:30");
-        projectquality.setId(1);
+        projectquality.setId(4);
         projectquality.setMethod1(1.0);
         projectquality.setMethod2(2.0);
-        projectquality.setProjectId(1);
+        projectquality.setProjectId(2);
         projectquality.setUserId("asdf");
         projectqualityDao.addProjectquality(projectquality);
     }
