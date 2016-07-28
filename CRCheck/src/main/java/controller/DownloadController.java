b221d936c4cb37b0179ad8022b0b7d3ee7828b67
ldc14 <ldc14@software.nju.edu.cn>
2016-07-29 04:29:58 +0800

    下载修改

--- CRCheck/src/main/java/controller/DownloadController.java
+++ CRCheck/src/main/java/controller/DownloadController.java
@@ -1,12 +1,16 @@
 package controller;
 
+import model.FileModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import service.FileService;
+import serviceImpl.FileServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.List;
 
 @Controller
 public class DownloadController {
@@ -29,9 +33,20 @@ public class DownloadController {
 		}
 		BufferedInputStream bis = null;
 		BufferedOutputStream bos = null;
-	
+
 		String ctxPath = request.getSession().getServletContext().getRealPath("/") + "ProjectResources/";
-		String downLoadPath = ctxPath + fileName;
+
+		String getCompressedPath=ctxPath+"ProjectCompressedFile/"+fileName;
+
+		FileService fileService=new FileServiceImpl();
+		List<FileModel> list= fileService.getDir(getCompressedPath);
+		String downLoadPath ="";
+		if(list.size()>0){
+			FileModel f=list.get(0);
+			downLoadPath=f.getContent();
+		}
+
+//		String downLoadPath = ctxPath + fileName;
 		System.out.println(downLoadPath);
 		try {
 			long fileLength = new File(downLoadPath).length();
@@ -62,7 +77,7 @@ public class DownloadController {
 				}
 		}
 		return null;
-	
+
 	}
 	
 }
