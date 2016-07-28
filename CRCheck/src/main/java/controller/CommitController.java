7c6499905bdbc59635a531e35018c496cee50422
zs14 <zs14@software.nju.edu.cn>
2016-07-28 20:38:10 +0800

    CommitController

--- CRCheck/src/main/java/controller/CommitController.java
+++ CRCheck/src/main/java/controller/CommitController.java
@@ -1,12 +1,16 @@
 package controller;
 
 import Dao.AttendanceDao;
+import enums.UniversalState;
+import model.CommitRecordModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.AttendanceService;
+import service.CommitService;
 import serviceImpl.AttendanceServiceImpl;
+import serviceImpl.CommitServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,6 +19,31 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class CommitController {
+
+    //提交每次评审的行数，用时等
+    @RequestMapping(value = "/commit")
+    @ResponseBody
+    public String commit(HttpServletRequest request){
+        //获得信息
+        String userId = request.getParameter("userId");
+        int projectId = Integer.parseInt(request.getParameter("projectId"));
+        int codeLine = Integer.parseInt(request.getParameter("codeLine"));
+        String reviewType = request.getParameter("reviewType");
+        int time = Integer.parseInt(request.getParameter("time"));
+        //转换信息
+        CommitRecordModel model = new CommitRecordModel();
+        model.setUserId(userId);
+        model.setProjectId(projectId);
+        model.setCodeLine(codeLine);
+        model.setReviewType(reviewType);
+        model.setTime(time);
+        //调用接口
+        CommitService service=new CommitServiceImpl();
+        UniversalState state=service.commit(model);
+        if(state==UniversalState.SUCCESS)
+            return "SUCCESS";
+        return "FAIL";
+    }
 
     @RequestMapping(value = "/checkReviewState", method = RequestMethod.POST)
     @ResponseBody
