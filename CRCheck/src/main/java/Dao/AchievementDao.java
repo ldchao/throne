6f63c94bb1c9b9f2b4c67af9047618dfe6a19239
lxbin14 <18813632558@163.com>
2016-08-16 12:48:49 +0800

    test

--- CRCheck/src/main/java/Dao/AchievementDao.java
+++ CRCheck/src/main/java/Dao/AchievementDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * Created by mm on 2016/7/28.
+
  */
 public interface AchievementDao {
     //need the "userId" , "type" and "value" ,it will return a list of "Achievement" whose value is satisfactory and the "User" doesn't have
